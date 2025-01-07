56818adde68c27c33c805814faf283d4c26703bc
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-01-07 19:25:29 +0600

    all the section components for the root page imported

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
+import ClientExperienceSection from "@/components/home/Client-Experiences/ClientExperienceSection";
+import EventSection from "@/components/home/EventSection";
+import ExploreEventSection from "@/components/home/ExploreEventSection";
+import FAQSection from "@/components/home/FAQ-Section/FaqSection";
+import MemoricaJourneySection from "@/components/home/MemoricaJourneySection";
+import ServiceSection from "@/components/home/ServiceSection";
+import WelcomeSection from "@/components/home/WelcomeSection";
+import WhyMemoricaSection from "@/components/home/WhyMemoricaSection";
 import React from "react";
 
 export default function Page() {
-  return <p>Memorica</p>;
+  return (
+    <div>
+      <WelcomeSection />
+      <EventSection />
+      <ServiceSection />
+      <ExploreEventSection />
+      <WhyMemoricaSection />
+      <ClientExperienceSection />
+      <MemoricaJourneySection />
+      <FAQSection />
+    </div>
+  );
 }
