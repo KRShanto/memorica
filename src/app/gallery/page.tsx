bf728a45a843ac57be4388249b2c68e5aba622f5
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-01-15 19:24:19 +0600

    responsiveness added to the gallery page

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Page() {
   return (
-    <div className="h-screen w-full grid gap-10 mt-5 mb-10 place-content-center place-items-center">
+    <div className="md:h-screen w-full grid gap-10 mt-5 mb-10 place-content-center place-items-center">
       {/* Header Section */}
       <div className="grid place-content-center place-items-center text-center w-full">
-        <h1 className="text-4xl tracking-wider font-bold">
+        <h1 className="text-2xl md:text-4xl tracking-wider font-bold">
           Every Frame is a Cherished Highlight
         </h1>
         <p className="mt-2 text-gray-600 text-sm w-2/4">
@@ -16,34 +16,34 @@ export default function Page() {
       </div>
 
       {/* Gallery Section */}
-      <div className="grid grid-cols-12 grid-rows-8 gap-4 h-full w-11/12">
+      <div className="grid grid-flow-row md:grid-cols-12 md:grid-rows-8 gap-4 h-full w-11/12">
         {/* Row 1 */}
         <div className="w-full h-40 flex justify-center items-center col-span-6 row-span-2 rounded-xl drop-shadow-md border-2 bg-blue-100">
           Large
         </div>
-        <div className="w-full h-40 flex justify-center items-center col-span-3 row-span-2 rounded-xl drop-shadow-md border-2 bg-green-100">
+        <div className="hidden w-full md:h-40 md:flex justify-center items-center md:col-span-3 md:row-span-2 rounded-xl drop-shadow-md border-2 bg-green-100">
           Small
         </div>
-        <div className="w-full h-40 flex justify-center items-center col-span-3 row-span-2 rounded-xl drop-shadow-md border-2 bg-green-100">
+        <div className="hidden w-full md:h-40 md:flex justify-center items-center md:col-span-3 md:row-span-2 rounded-xl drop-shadow-md border-2 bg-green-100">
           Small
         </div>
 
         {/* Row 2 */}
-        <div className="w-full h-40 flex justify-center items-center col-span-3 row-span-2 rounded-xl drop-shadow-md border-2 bg-yellow-100">
+        <div className="hidden w-full md:h-40 md:flex justify-center items-center md:col-span-3 md:row-span-2 rounded-xl drop-shadow-md border-2 bg-yellow-100">
           Small
         </div>
         <div className="w-full h-40 flex justify-center items-center col-span-6 row-span-2 rounded-xl drop-shadow-md border-2 bg-red-100">
           Large
         </div>
-        <div className="w-full h-40 flex justify-center items-center col-span-3 row-span-2 rounded-xl drop-shadow-md border-2 bg-blue-100">
+        <div className="hidden w-full md:h-40 md:flex justify-center items-center md:col-span-3 md:row-span-2 rounded-xl drop-shadow-md border-2 bg-blue-100">
           small
         </div>
 
         {/* Row 3 */}
-        <div className="w-full h-40 flex justify-center items-center col-span-3 row-span-2 rounded-xl drop-shadow-md border-2 bg-purple-100">
+        <div className="hidden w-full md:h-40 md:flex justify-center items-center md:col-span-3 md:row-span-2 rounded-xl drop-shadow-md border-2 bg-purple-100">
           Small
         </div>
-        <div className="w-full h-40 flex justify-center items-center col-span-3 row-span-2 rounded-xl drop-shadow-md border-2 bg-orange-100">
+        <div className="hidden w-full md:h-40 md:flex justify-center items-center md:col-span-3 md:row-span-2 rounded-xl drop-shadow-md border-2 bg-orange-100">
           Small
         </div>
         <div className="w-full h-40 flex justify-center items-center col-span-6 row-span-2 rounded-xl drop-shadow-md border-2 bg-blue-100">
@@ -54,10 +54,10 @@ export default function Page() {
         <div className="w-full h-40 flex justify-center items-center col-span-6 row-span-2 rounded-xl drop-shadow-md border-2 bg-blue-100">
           Large
         </div>
-        <div className="w-full h-40 flex justify-center items-center col-span-3 row-span-2 rounded-xl drop-shadow-md border-2 bg-green-100">
+        <div className="w-full h-40 flex justify-center items-center col-span-6 md:col-span-3 row-span-2 rounded-xl drop-shadow-md border-2 bg-green-100">
           Small
         </div>
-        <div className="w-full h-40 flex justify-center items-center col-span-3 row-span-2 rounded-xl drop-shadow-md border-2 bg-green-100">
+        <div className="hidden w-full md:h-40 md:flex justify-center items-center md:col-span-3 md:row-span-2 rounded-xl drop-shadow-md border-2 bg-green-100">
           Small
         </div>
       </div>
