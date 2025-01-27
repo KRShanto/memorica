d4535cbd2e3414a91ba5c91ce5d421df61bd9ba1
KR Shanto <krshanto2005@gmail.com>
2025-01-27 18:17:30 +0600

    disable no-explicit-any rule in ESLint and update ClientExperienceSection to use 'any' type for people

--- src/components/home/Client-Experiences/ClientExperienceSection.tsx
+++ src/components/home/Client-Experiences/ClientExperienceSection.tsx
@@ -1,15 +1,18 @@
-import dynamic from 'next/dynamic';
-import {people} from '@/components/Dummy-Data/people'
+import dynamic from "next/dynamic";
+import { people } from "@/components/Dummy-Data/people";
 
-const DynamicSwiper = dynamic(() => import('./DynamicSwiper'), {
+const DynamicSwiper = dynamic(() => import("./DynamicSwiper"), {
   // ssr: false, // Disable SSR for this component
 });
 
 const ClientExperienceSection = () => {
   return (
     <div className="p-3 grid place-content-center place-items-center">
-      <h1 className="text-center text-2xl font-semibold tracking-widest w-3/4 mb-8">Client Experience That Inspire Us To Keep Creating Excellence</h1>
-      <DynamicSwiper people={people} />
+      <h1 className="text-center text-2xl font-semibold tracking-widest w-3/4 mb-8">
+        Client Experience That Inspire Us To Keep Creating Excellence
+      </h1>
+      {/* TODO: fix typescript */}
+      <DynamicSwiper people={people as any} />
     </div>
   );
 };
