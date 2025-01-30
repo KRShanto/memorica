46bdecfc937be22089f063d48ae6cf81e552f1a5
KR Shanto <krshanto2005@gmail.com>
2025-01-30 19:13:26 +0600

    refactor: move JourneyForm component to contact directory

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Metadata } from "next";
-import JourneyForm from "@/components/about-us/JourneyForm";
+import JourneyForm from "@/components/contact/JourneyForm";
 
 export const metadata: Metadata = {
   title: "Contact Us",
