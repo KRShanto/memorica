bbf5c7cf0ca8b3e2749b7abb418a0db171e6917f
KR Shanto <krshanto2005@gmail.com>
2025-02-09 21:03:17 +0600

    feat: add .htaccess for URL rewriting and update image paths in carousel components

--- src/components/Carrossel/Slider.tsx
+++ src/components/Carrossel/Slider.tsx
@@ -1,32 +1,28 @@
-'use client'
-import { ImageContext } from '@/context/ImageContext'
-import Image from 'next/image'
-import { useContext } from 'react'
-import 'swiper/css'
-import 'swiper/css/autoplay'
-import 'swiper/css/effect-coverflow'
-import { Autoplay, EffectCoverflow } from 'swiper/modules'
-import { Swiper, SwiperSlide } from 'swiper/react'
+"use client";
+import { ImageContext } from "@/context/ImageContext";
+import Image from "next/image";
+import { useContext } from "react";
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/effect-coverflow";
+import { Autoplay, EffectCoverflow } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 export default function CarouselThreeD() {
-  const { activeImageIndex, setActiveImageIndex } = useContext(ImageContext)
+  const { activeImageIndex, setActiveImageIndex } = useContext(ImageContext);
   const images = [
-    'https://swiperjs.com/demos/images/nature-1.jpg',
-    'https://swiperjs.com/demos/images/nature-2.jpg',
-    'https://swiperjs.com/demos/images/nature-3.jpg',
-    'https://swiperjs.com/demos/images/nature-4.jpg',
-    'https://swiperjs.com/demos/images/nature-5.jpg',
-    'https://swiperjs.com/demos/images/nature-6.jpg',
-    'https://swiperjs.com/demos/images/nature-7.jpg',
-    'https://swiperjs.com/demos/images/nature-8.jpg',
-    'https://swiperjs.com/demos/images/nature-9.jpg',
-  ]
+    "/hero/hero1.png",
+    "/hero/hero2.png",
+    "/hero/hero3.png",
+    "/hero/hero4.png",
+    "/hero/hero5.png",
+  ];
 
   return (
     <>
-      <div className='w-full flex items-center justify-center'>
+      <div className="w-full flex items-center justify-center ">
         <Swiper
-          effect={'coverflow'}
+          effect={"coverflow"}
           grabCursor={true}
           centeredSlides={true}
           slidesPerView={5}
@@ -45,21 +41,21 @@ export default function CarouselThreeD() {
             disableOnInteraction: false,
           }}
           modules={[EffectCoverflow, Autoplay]}
-          className='w-3/4 h-52'
+          className="w-full h-64"
         >
           {images.map((src, index) => (
             <SwiperSlide
               key={index}
               className={`relative w-28 h-52 rounded-lg shadow-lg overflow-hidden transition-all ${
-                activeImageIndex === index ? 'border-4 border-white' : ''
+                activeImageIndex === index ? "border-4 border-white" : ""
               }`}
             >
-              <div className='relative w-full h-full'>
+              <div className="relative w-full h-full">
                 <Image
                   src={src}
                   alt={`Slide ${index + 1}`}
                   fill
-                  className='object-cover rounded-lg'
+                  className="object-cover rounded-lg"
                 />
               </div>
             </SwiperSlide>
@@ -67,5 +63,5 @@ export default function CarouselThreeD() {
         </Swiper>
       </div>
     </>
-  )
+  );
 }
--- src/components/home/WelcomeSection.tsx
+++ src/components/home/WelcomeSection.tsx
@@ -1,76 +1,72 @@
-'use client'
-import { ImageContext } from '@/context/ImageContext'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useContext } from 'react'
-import Slider from '../Carrossel/Slider'
+"use client";
+import { ImageContext } from "@/context/ImageContext";
+import Image from "next/image";
+import Link from "next/link";
+import { useContext } from "react";
+import Slider from "../Carrossel/Slider";
 
 export default function WelcomeSection() {
-  const { activeImageIndex } = useContext(ImageContext)
+  const { activeImageIndex } = useContext(ImageContext);
   const sliderImages = [
-    'https://swiperjs.com/demos/images/nature-1.jpg',
-    'https://swiperjs.com/demos/images/nature-2.jpg',
-    'https://swiperjs.com/demos/images/nature-3.jpg',
-    'https://swiperjs.com/demos/images/nature-4.jpg',
-    'https://swiperjs.com/demos/images/nature-5.jpg',
-    'https://swiperjs.com/demos/images/nature-6.jpg',
-    'https://swiperjs.com/demos/images/nature-7.jpg',
-    'https://swiperjs.com/demos/images/nature-8.jpg',
-    'https://swiperjs.com/demos/images/nature-9.jpg',
-  ]
+    "/hero/hero1.png",
+    "/hero/hero2.png",
+    "/hero/hero3.png",
+    "/hero/hero4.png",
+    "/hero/hero5.png",
+  ];
   return (
     <>
-      <div className='relative min-h-screen bg-gray-700/100 grid place-content-center place-items-center gap-5'>
+      <div className="relative min-h-screen bg-gray-700/100 grid place-content-center place-items-center gap-5">
         {/* Blurred Background */}
-        <div className='absolute inset-0 z-0'>
+        <div className="absolute inset-0 z-0">
           <Image
             src={sliderImages[activeImageIndex]}
-            alt='Welcome Background'
-            layout='fill'
-            objectFit='cover'
+            alt="Welcome Background"
+            layout="fill"
+            objectFit="cover"
             priority
-            className='blur-[6px]'
+            className="blur-[6px]"
           />
         </div>
 
         {/* Content */}
-        <div className='relative z-10 text-white text-center px-4'>
-          <h1 className='text-7xl font-antic mb-4 leading-tight'>
+        <div className="relative z-10 text-white text-center px-4">
+          <h1 className="text-7xl font-antic mb-4 leading-tight">
             Crafting Moments <br /> Creating Memories
           </h1>
-          <p className='text-2xl mb-6 font-jakarta'>
+          <p className="text-2xl mb-6 font-jakarta">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
             Ullam voluptatibus a quia ipsa est.
           </p>
         </div>
 
         {/* Satisfied Clients Section */}
-        <div className='relative z-10'>
-          <div className='flex justify-center items-center'>
+        <div className="relative z-10">
+          <div className="flex justify-center items-center">
             {/* Profile Avatars */}
-            <div className='flex -m-1'>
-              {['female1.png', 'male1.png', 'female2.png'].map((img, index) => (
+            <div className="flex -m-1">
+              {["female1.png", "male1.png", "female2.png"].map((img, index) => (
                 <div
                   key={index}
-                  className='md:w-8 md:h-8 sm:w-6 sm:h-6 rounded-full overflow-hidden -m-1'
+                  className="md:w-8 md:h-8 sm:w-6 sm:h-6 rounded-full overflow-hidden -m-1"
                 >
                   <Image
                     src={`/welcome-section/${img}`}
                     alt={`Avatar ${index + 1}`}
                     width={40} // Tailwind's `w-8` = 32px
                     height={40} // Tailwind's `h-8` = 32px
-                    objectFit='cover'
+                    objectFit="cover"
                   />
                 </div>
               ))}
             </div>
 
             {/* Count & Text */}
-            <div className='flex items-center -m-1'>
-              <div className='w-8 h-8 p-4 flex items-center justify-center rounded-full bg-[#8130b6] text-white text-xs'>
+            <div className="flex items-center -m-1">
+              <div className="w-8 h-8 p-4 flex items-center justify-center rounded-full bg-[#8130b6] text-white text-xs">
                 100+
               </div>
-              <h3 className='ml-2 text-white text-sm font-medium'>
+              <h3 className="ml-2 text-white text-sm font-medium">
                 Satisfied Clients
               </h3>
             </div>
@@ -78,17 +74,17 @@ export default function WelcomeSection() {
         </div>
 
         <Link
-          href={'tel:1234567890'}
-          className='relative border px-2 py-1 z-10 bg-white text-[#8130b6] rounded-lg text-md mt-3 font-medium'
+          href={"tel:1234567890"}
+          className="relative border px-2 py-1 z-10 bg-white text-[#8130b6] rounded-lg text-md mt-3 font-medium"
         >
           Book A Call
         </Link>
 
         {/* Slider Section */}
-        <div className='w-full absolute bottom-12 left-1/2 -translate-x-1/2'>
+        <div className="w-full absolute bottom-12 left-1/2 -translate-x-1/2">
           <Slider />
         </div>
       </div>
     </>
-  )
+  );
 }
